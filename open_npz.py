977c2a7ecf61395f6960ceef83d6d05848d6c66e
johnnyhff11 <johnny_huang@berkeley.edu>
2023-01-27 17:58:27 -0800

    change in open_npz.py

--- open_npz.py
+++ open_npz.py
@@ -1,9 +1,11 @@
-def open_npz(captured_data, arr_1, arr_2):
+def open_npz(captured_data, arr_0, arr_1):
     import numpy as np #Package needed for opening npz file!
     if type(captured_data) != 'str':
         package = f'{captured_data}'
 
     elif type(captured_data) = 'str':
         package = captured_data
+    else:
+        package = captured_data
         
-    npz_opened = np.load(package, arr_1, arr_2)
+    npz_opened = np.load(package, arr_0, arr_1)
